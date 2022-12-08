1f02cf2f7d986b6117eaf3f2947001e5f04bdba3
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-12-08 21:47:44 +0100

    dodat i drugi

--- v01/v01_bonus.jl
+++ v01/v01_bonus.jl
@@ -35,3 +35,25 @@ A = [1 4 -2 9 6; -1 0 0 3 7; 99 3 -3 4 7; 5 -6 0 -8 3; 1 2 3 4 5]
 a, b, c = primer(A, broj)
 
 
+
+# Zadatak sa konsultacija
+# Napisati funkciju koja kao ulazni parametar prima kvadratnu matricu A i skalar s, a kao povratne vrednosti vraća:
+# - skalar s, koji predstavlja srednju vrednost svih pozitivnih elemenata matrice A koji su deljivi sa skalarom s
+# - vektor p, koji se sastoji od elemenata sa glavne dijagonale matrice A koji su veću od zbira svih elemenata iz neparnih vrsta te matrice
+function zadatak1(A, s)
+	pozitivni = A[A .> 0]
+	deljivi = pozitivni[rem.(pozitivni, s) .== 0]
+	c = mean(dejlivi)
+
+	suma = sum(A[1:2:end, :])
+	dijagonala = diag(A)
+	p = dijagonala[dijagonala .> suma]
+
+	return c, p
+end
+
+# Poziv funkcije
+B = [3 1 4 9 6; -1 4623 23 3 7; 99 3 -3 4 7; 5 -6 0 -8 3; 1 2 3 4 5]
+broj = 4
+skalar, vektor = zadatak1(B, broj)
+
